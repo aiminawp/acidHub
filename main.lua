dd7c7833089e253dece9e606f784d46bb22fffa0
aiminawp <tutorialsandgameplay21@gmail.com>
2025-06-16 22:46:27 +0300

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+if game.GameId == 126884695634066 then
+    loadstring(game:HttpGet("http://raw.githubusercontent.com/aiminawp/skbidid17t87steotatuTEST/main/growAgarden.lua"))()
+elseif game.GameId == 2788229376 then
+    loadstring(game:HttpGet("http://raw.githubusercontent.com/aiminawp/skbidid17t87steotatuTEST/main/dahood.lua"))()
+end
