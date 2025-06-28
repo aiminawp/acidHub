63ed5e1ec00d9ee67ea7a8d6567215bba9ff05c0
aiminawp <tutorialsandgameplay21@gmail.com>
2025-06-28 12:46:03 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,4 +2,6 @@ if game.GameId == 7436755782 then
     loadstring(game:HttpGet("http://raw.githubusercontent.com/aiminawp/skbidid17t87steotatuTEST/main/growAgarden.lua"))()
 elseif game.GameId == 1008451066 then
     loadstring(game:HttpGet("http://raw.githubusercontent.com/aiminawp/skbidid17t87steotatuTEST/main/dahood.lua"))()
+elseif game.GameId == 6331920150
+    loadstring(game:HttpGet("http://raw.githubusercontent.com/aiminawp/skbidid17t87steotatuTEST/main/forsaken.lua"))()
 end
